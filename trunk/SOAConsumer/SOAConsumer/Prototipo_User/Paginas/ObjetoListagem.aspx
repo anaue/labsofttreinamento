607f0f306a631f7ea3038cd57d4cd50db2ce043f
leandrodvd <leandrodvd@66b7d206-f197-11dd-a295-0b7b0935bf94>
2009-02-06 10:50:20 +0000

    Start Point for Consumer

---
+++ trunk/SOAConsumer/SOAConsumer/Prototipo_User/Paginas/ObjetoListagem.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ObjetoListagem.aspx.cs" Inherits="Prototipo3C_Aplicacao.Paginas.ObjetoListagem" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>Untitled Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:ObjectDataSource ID="ObjectDataSource1" runat="server" 
+            SelectMethod="ObterObjetos" 
+            TypeName="Prototipo3C_Negocio.Controladores.ObjetoController">
+        </asp:ObjectDataSource>
+        <br />
+        <br />
+        <asp:GridView ID="GridView1" runat="server" AllowPaging="True" 
+            AutoGenerateColumns="False" CellPadding="4" DataSourceID="ObjectDataSource1" 
+            ForeColor="#333333" GridLines="None" Width="341px">
+            <FooterStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+            <RowStyle BackColor="#FFFBD6" ForeColor="#333333" />
+            <Columns>
+                <asp:CommandField ShowSelectButton="True" />
+                <asp:BoundField DataField="ObjetoId" HeaderText="ObjetoId" ReadOnly="True" 
+                    SortExpression="ObjetoId" />
+                <asp:BoundField DataField="Nome" HeaderText="Nome" SortExpression="Nome" />
+                <asp:BoundField DataField="Cor" HeaderText="Cor" SortExpression="Cor" />
+            </Columns>
+            <PagerStyle BackColor="#FFCC66" ForeColor="#333333" HorizontalAlign="Center" />
+            <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="Navy" />
+            <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+            <AlternatingRowStyle BackColor="White" />
+        </asp:GridView>
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ trunk/SOAConsumer/SOAConsumer/Prototipo_Workspace.dll/Controladores/ObjetoController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Web;
+using Prototipo3C_Entidade.Entidade;
+using Prototipo_Workspace.dll;
+
+namespace Prototipo3C_Negocio.Controladores
+{
+    public class ObjetoController
+    {
+
+        public IList<Prototipo3C_Entidade.Entidade.Objeto> ObterObjetos()
+        {
+            Prototipo_Workspace.dll.Patrimonio.Itens wsPatrimonio;
+            Prototipo_Workspace.dll.Patrimonio.Response response;
+            List<Prototipo3C_Entidade.Entidade.Objeto> lstObj;
+
+            lstObj = new List<Prototipo3C_Entidade.Entidade.Objeto>();
+            wsPatrimonio = new Prototipo_Workspace.dll.Patrimonio.Itens();
+            try
+            {
+
+                response = wsPatrimonio.ObterRegistros();
+                if (response != null)
+                {
+
+                    foreach (Prototipo_Workspace.dll.Patrimonio.Objeto obj in response.ColecaoObjetos)
+                    {
+                        lstObj.Add(new Prototipo3C_Entidade.Entidade.Objeto(obj.ObjId,obj.Campo,obj.Valor));
+                    }
+
+
+                }
+            }
+            catch(Exception ex)
+            {
+                return lstObj;
+            }
+
+
+            return lstObj;
+        }
+
+        public bool CadastrarObjeto(Objeto objeto)
+        {
+            Prototipo_Workspace.dll.Patrimonio.Itens wsPatrimonio;
+            Prototipo_Workspace.dll.Patrimonio.Response response;
+            Prototipo_Workspace.dll.Patrimonio.Request request;
+
+            wsPatrimonio = new Prototipo_Workspace.dll.Patrimonio.Itens();
+            request = new Prototipo_Workspace.dll.Patrimonio.Request();
+
+            request.ColecaoObjetos = new Prototipo_Workspace.dll.Patrimonio.Objeto[1];
+            request.ColecaoObjetos[0] = new Prototipo_Workspace.dll.Patrimonio.Objeto();
+
+            request.ColecaoObjetos[0].Campo = objeto.Nome;
+            request.ColecaoObjetos[0].ObjId = objeto.ObjetoId;
+            request.ColecaoObjetos[0].Valor = objeto.Cor;
+            try
+            {
+                response = wsPatrimonio.IncluirResgistro(request);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            return (response != null && response.Cod_status == 200);
+        }
+
+
+    }
+}
